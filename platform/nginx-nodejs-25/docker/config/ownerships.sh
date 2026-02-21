c9073cc96259956e671634866aec70c99308cce7
Pablo Ripoll <pabloripoll.it@gmail.com>
2026-02-21 12:23:18 +0100

    refactor: bash script relocated as latest platform series have

--- platform/nginx-nodejs-25/docker/config/ownerships.sh
+++ platform/nginx-nodejs-25/docker/config/ownerships.sh
@@ -12,12 +12,9 @@ if [ -z "${USER}" ] || [ -z "${GROUP}" ]; then
 fi
 
 # Update Supervisord configuration
-file=./supervisord.conf
-if [ -e "$file" ]; then
+if [ ! -e supervisor/supervisord.conf ]; then
     # Update chown in [unix_http_server] section
     sed -i "s/^chown=.*/chown=${USER}:${GROUP}/" ./docker/config/supervisor/supervisord.conf
 else
-    # Create file and update chown in [unix_http_server] section
-    cp ./docker/config/supervisor/supervisord.conf.sample ./docker/config/supervisor/supervisord.conf
-    sed -i "s/^chown=.*/chown=${USER}:${GROUP}/" ./docker/config/supervisor/supervisord.conf
+    echo "WARNING: ./docker/config/supervisor/supervisord.conf not found"
 fi
